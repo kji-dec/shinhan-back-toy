6ea474f47e188763595ee7632403bf16838ef0c6
kji-dec <dec22sag@gmail.com>
2023-01-25 16:27:38 +0900

    feat: Add trailing comma

--- shinhantoy/shinhantoy/urls.py
+++ shinhantoy/shinhantoy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/order', include('order.urls'))
+    path('api/order', include('order.urls')),
 ]
