91c6da0ada2ffeb31fc3d10f272c6e12cb407495
kji-dec <dec22sag@gmail.com>
2023-01-25 16:41:01 +0900

    feat: Modify get_queryset
    modified get_queryset to order by "-id"

--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -13,7 +13,7 @@ class OrderListView(
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        return Order.objects.all()
+        return Order.objects.all().order_by('-id')
 
     def get(self, request, *args, **kwargs):
         return self.list(request, args, kwargs)
