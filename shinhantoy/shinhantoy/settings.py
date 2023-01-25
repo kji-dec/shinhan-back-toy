2c6839894aba0a2c5de768674605f364db135438
kji-dec <dec22sag@gmail.com>
2023-01-25 16:07:39 +0900

    conf: Add cors, pagination, etc
    added some configurations.
    pagination, cors, add app

--- shinhantoy/shinhantoy/settings.py
+++ shinhantoy/shinhantoy/settings.py
@@ -53,9 +53,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'order.apps.OrderConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +141,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+if DEBUG: # cors allow all origins only in debug env
+    CORS_ALLOW_ALL_ORIGINS = True
