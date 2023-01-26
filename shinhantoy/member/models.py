359530abf529d3381339b8df2f1f3ac180f7ae75
kji-dec <dec22sag@gmail.com>
2023-01-26 10:40:39 +0900

    feat: Add status field into member
    added status field into member to inform about user

--- shinhantoy/member/models.py
+++ shinhantoy/member/models.py
@@ -5,6 +5,14 @@ from django.contrib.auth.models import AbstractUser
 
 class Member(AbstractUser):
     tel = models.CharField(max_length=32, null=True, blank=True, verbose_name="연락처")
+    status = models.CharField(max_length=16, default="일반",
+        choices=(
+            ('일반', '일반'),
+            ('탈퇴', '탈퇴'),
+            ('휴면', '휴면'),
+        )
+    )
+
     REQUIRED_FIELDS = ["tel"]
     class Meta:
         db_table = 'shinhan_member'
